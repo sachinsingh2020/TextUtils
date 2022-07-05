5bd03b686e2a4e855523faba1c221de4e4add633
Sachin Singh <sachinthegreat1001@gmail.com>
2022-07-05 11:21:50 +0530

    First
    First commit

---
+++ precache-manifest.a5c9ece62a67a0cff62cb298a2e2895c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65b305122ab0a574f93e",
+    "url": "/static/css/main.1c5add29.chunk.css"
+  },
+  {
+    "revision": "65b305122ab0a574f93e",
+    "url": "/static/js/main.65b30512.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "1d27a2cb9eb27a6df6e5f1857538c3ff",
+    "url": "/index.html"
+  }
+];
